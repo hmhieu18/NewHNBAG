8f67ef1df44e49a1e019fc1f381cbe4bd8b74d33
hieu <hmhieu18@apcs.vn>
2020-08-20 22:49:46 +0700

    commit

--- app/src/main/java/com/example/hnbag/MainActivity.java
+++ app/src/main/java/com/example/hnbag/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.hnbag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button eat;
+    private Button drinks;
+    private Button favorite;
+    private Button photo;
+    private Button search;
+    private EditText search_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
+        initComponent();
+    }
+
+    private void initComponent() {
+        eat = (Button) findViewById(R.id.eat);
+        drinks = (Button) findViewById(R.id.eat);
+        favorite = (Button) findViewById(R.id.eat);
+        photo = (Button) findViewById(R.id.eat);
+        search_bar = (EditText) findViewById(R.id.search_bar);
+        search = (Button) findViewById(R.id.search);
+    }
+
+    public void searchClicked(View view) {
+        Intent myIntent = new Intent(MainActivity.this, searchResult.class);
+        myIntent.putExtra("key", search_bar.getText()); //Optional parameters
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    public void photoClicked(View view) {
+    }
+
+    public void favoriteClicked(View view) {
+    }
+
+    public void drinksClicked(View view) {
+    }
+
+    public void eatClicked(View view) {
+
     }
 }
