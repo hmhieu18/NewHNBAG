7e536fc3165239bd72b2645b74465934cc0a8b08
ntdat2000 <ntdat18@apcs.vn>
2020-08-21 01:26:34 +0700

    hello

--- app/src/main/java/com/example/hnbag/searchResult.java
+++ app/src/main/java/com/example/hnbag/searchResult.java
@@ -2,7 +2,9 @@ package com.example.hnbag;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +12,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class searchResult extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,6 +34,8 @@ public class searchResult extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        new ReadJSONObject().execute("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos%2Cformatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyDk2Z5H8EiJNIsHWrb7-fXXDnhbPqjIfbI&fbclid=IwAR3t1ceYnuwQ1WXgz3CUb9t95sdouCssCZqwjYfdRLtuAW5Y3_UuSW0Mv6w");
     }
 
     /**
@@ -43,4 +56,44 @@ public class searchResult extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    private class ReadJSONObject extends AsyncTask<String,Void,String>
+    {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content=new StringBuilder();
+            try {
+                URL url=new URL(strings[0]);
+                InputStreamReader inputStreamReader=new InputStreamReader(url.openConnection().getInputStream());
+                BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+
+                String line="";
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    content.append(line);
+                }
+                bufferedReader.close();
+
+            } catch (MalformedURLException e) {
+            } catch (IOException e) {
+            }
+            return content.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            JSONObject object= null;
+            try {
+                object = new JSONObject(s);
+                JSONObject  b = object.getJSONObject("rates");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(searchResult.this,s,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
